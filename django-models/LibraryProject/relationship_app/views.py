b9ecd744908eb080571151b46bfa559faccb4a66
Osayande <michaeltakeover@gmail.com>
2025-07-26 16:03:33 +0100

    updating

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Book
-from django.views.generic import ListView
+#from django.views.generic import ListView
 # Create your views here.
 
 
@@ -9,9 +9,9 @@ def book_list(request):
     context = {'book_list': books}
     return render(request, 'books/book_list.html', context)
 
-
+'''
 class BookListView(ListView):
     model = Book
     template_name = 'list_books.html'
     context_object_name = 'book'
-
+'''
