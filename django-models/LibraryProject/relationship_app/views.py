c72b035a24ca7fbb1ec2bf7cf77a993224ba145e
Osayande <michaeltakeover@gmail.com>
2025-07-26 16:15:24 +0100

    updating

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,7 +7,7 @@ from .models import Book
 def book_list(request):
     books = Book.objects.all()
     context = {'book_list': books}
-    return render(request, 'relationship_app/book_list.html', context)
+    return render(request, 'relationship_app/list_books.html', context)
 
 '''
 class BookListView(ListView):
