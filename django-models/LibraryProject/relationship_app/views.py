4b7e341fd1413873a5e148fa9339d64f7b1c209c
Osayande <michaeltakeover@gmail.com>
2025-07-26 13:58:41 +0100

    updating views and urls

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from .models import Book
+from django.views.generic import ListView
 # Create your views here.
+
+
+def book_list(request):
+    books = Book.objects.all()
+    context = {'book_list': books}
+    return render(request, 'books/book_list.html', context)
+
+
+class BookListView(ListView):
+    model = Book
+    tempelate_name = 'list_books.html'
+    context_object_name = 'book'
+
