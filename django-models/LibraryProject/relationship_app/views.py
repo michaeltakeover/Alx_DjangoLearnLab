00b53c8eca19fec3bbb3ea52483964dd04f47547
Osayande <michaeltakeover@gmail.com>
2025-07-28 20:58:17 +0100

    update

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,7 +6,7 @@ from .models import Book
 # Create your views here.
 
 
-def book_list(request):
+def list_books(request):
     books = Book.objects.all().select_related('author')
     context = {'books': books}
     return render(request, 'relationship_app/list_books.html', context)
